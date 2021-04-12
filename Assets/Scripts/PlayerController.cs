a64189a336d82acbbe8d341de6a8e88d93cd130f
sehers-code <sehersevik00@gmail.com>
2021-04-12 22:50:09 +0300

    Initial commit. <3

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5;
+    public TextMeshProUGUI countText;
+    public GameObject Congrats;
+    private Rigidbody rb;
+    public AudioSource source;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        Congrats.SetActive(false);
+        setCountText();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void setCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+
+        if(count >= 13)
+        {
+            Congrats.SetActive(true);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            source.Play();
+            count += 1;
+            setCountText();
+        }
+    }
+}
